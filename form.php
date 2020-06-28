c3a58d4407544347f8a666ca7b3713b42f7b2add
Nur hilda Pragita Cahayni <62729014+hildagita@users.noreply.github.com>
2020-06-28 21:29:11 +0700

    Create form.php

---
+++ form.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form Pendaftaran</title>
+</head>
+<body>
+	<center><h1>Form Pendaftaran</h1></center>
+ 
+	<?php 
+	if(isset($_GET['nama'])){
+		if($_GET['nama'] == "kosong"){
+			echo "<h4 style='color:red'>Nama Belum Di Masukkan !</h4>";
+		}
+	}
+	if(isset($_GET['email'])){
+		if($_GET['email'] == "kosong"){
+			echo "<h4 style='color:red'>Email Belum Di Masukkan !</h4>";
+		}
+	}
+	if(isset($_GET['password'])){
+		if($_GET['password'] == "kosong"){
+			echo "<h4 style='color:red'>password Belum Di Masukkan !</h4>";
+		}
+	}
+	?>
+
+	<form action="cek.php" method="post">
+		<table align="center">
+			<tr>
+				<td>Nama</td>
+				<td><input type="text" name="nama"></td>
+			</tr><tr>
+				<td>Email</td>
+				<td><input type="text" name="email"></td>
+			</tr><tr>
+				<td>Password</td>
+				<td><input type="password" name="password"></td>
+			</tr><tr>
+				<td align="center"><input type="submit" value="Cek"></td>
+			</tr>						
+		</table>
+ </center>
+</body>
+</html>
